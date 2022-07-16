c8b349e6be9974693c0396257efe42c1223d41ea
又来了个玩家 <109119269+GuoYufan@users.noreply.github.com>
2022-07-17 01:13:52 +0800

    Update try_220624_最大公约数.py

--- try/try_220619_cook/try_220624_小数与分数/try_220624_最大公约数/try_220624_最大公约数.py
+++ try/try_220619_cook/try_220624_小数与分数/try_220624_最大公约数/try_220624_最大公约数.py
@@ -4,18 +4,36 @@ def 获取最大公约数(a,b):
     smaller,bigger=(a,b) if a<=b else (b,a)
     
     while True:
-        now=bigger%smaller
-        if now==0:break
-        bigger,smaller=smaller,now
+        remainder=bigger%smaller
+        if remainder==0:break
+        bigger,smaller=smaller,remainder
 
-    return smaller
+    return abs(smaller)
 
-def 获取最简分数(a,b):
-    最大公约数=获取最大公约数(a,b)
-    分子约分后=a//最大公约数
-    分母约分后=b//最大公约数
+
+#单参数版（常用）——接收分数的字符串形式版——返回同形式
+def 获取最简分数(原分数的字符串形式):
+    原分子,原分母=map(int,原分数的字符串形式.split("/"))
+    
+    最大公约数=获取最大公约数(原分子,原分母)    
+    分子约分后=原分子//最大公约数
+    分母约分后=原分母//最大公约数
     
     return f"{分子约分后}/{分母约分后}"
+
+    
+def 获取最简分数_多参版(原分子,原分母):
+    原分数的字符串形式="/".join(map(str,(原分子,原分母)))
+    
+    return 获取最简分数(原分数的字符串形式)
+    
+
+#单参数版——接收分数的列表形式版——返回同形式
+def 分数约分(原分数的列表形式):
+    最简分数的字符串形式=获取最简分数("/".join(map(str,原分数的列表形式)))
+    分数约分后的字符串列表形式=最简分数的字符串形式.split("/")
+    
+    return 分数约分后的字符串列表形式
 
 
 def 试用():
@@ -26,7 +44,8 @@ def 试用():
         except:continue
         break
     print("\n%s和%s的最大公约数是%s"%(a,b,获取最大公约数(a,b)))
-    print("最简分数是%s"%获取最简分数(a,b))
+    print("最简分数是%s"%获取最简分数_多参版(a,b))
+
 
 def main():
     while True:试用()
