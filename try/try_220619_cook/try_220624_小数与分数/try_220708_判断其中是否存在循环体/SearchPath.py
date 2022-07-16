9924f797f60073b51153eb1942f142e2da1e7470
又来了个玩家 <109119269+GuoYufan@users.noreply.github.com>
2022-07-17 02:18:29 +0800

    Add files via upload

---
+++ try/try_220619_cook/try_220624_小数与分数/try_220708_判断其中是否存在循环体/SearchPath.py
@@ -0,0 +1,18 @@
+import os
+here=__file__
+#当设置为1 为回退到最底层目录
+回退到上几级目录=5
+for i in range(回退到上几级目录):
+    here=os.path.dirname(here)
+del 回退到上几级目录,i
+
+import sys
+sys.path.append(
+	here+os.sep.join(
+	(
+	str(),"common",
+	"module",
+	"PKP","Stay","AmusementPark",
+	)
+))
+del os,sys,here
