ae8adfcc8989508726db0190383af5caec2cc70e
又来了个玩家 <109119269+GuoYufan@users.noreply.github.com>
2022-07-17 01:06:03 +0800

    Update try_220701_分数相加减.py

--- try/try_220619_cook/try_220624_小数与分数/try_220701_分数相加减/try_220701_分数相加减.py
+++ try/try_220619_cook/try_220624_小数与分数/try_220701_分数相加减/try_220701_分数相加减.py
@@ -1,21 +1,5 @@
-def 获取最大公约数(a,b):
-    #这里要加括号形成元组 不然被逗号分隔了
-    #如此 右操作数有三个 左边只有两个 就对不上了  
-    smaller,bigger=(a,b) if a<=b else (b,a)
-    
-    while True:
-        now=bigger%smaller
-        if now==0:break
-        bigger,smaller=smaller,now
-
-    return smaller
-
-def 获取最简分数(a,b):
-    最大公约数=获取最大公约数(a,b)
-    分子约分后=a//最大公约数
-    分母约分后=b//最大公约数    
-    
-    return f"{分子约分后}/{分母约分后}"
+from SearchPath import *
+from try_Using_最大公约数 import *
 
 
 def 获取最小公倍数(a,b):
@@ -24,21 +8,33 @@ def 获取最小公倍数(a,b):
     #return a/c*b/c*c
     return int(a*b/c)
 
-   
-def 分数相加减(分数一,分数二,运算法):
+
+#分数在字符串形式与列表形式之间的转换
+def 分数形式转换(分数,转换为=list):
+    if type(分数)==转换为:return 分数
+    
+    if isinstance(分数,list) and 转换为==str:
+        return "/".join(map(str,分数))
+    elif isinstance(分数,str) and 转换为==list:
+        return list(map(int,分数.split("/")))
+    
+    raise TypeError("❌无法转换")
+    
+
+def 分数相加减(分数一,分数二,运算法="+"):
+    分数一,分数二=map(分数形式转换,(分数一,分数二))
+    
     分子一,分母一=分数一[0],分数一[1]
     分子二,分母二=分数二[0],分数二[1]
     
     共同分母=获取最小公倍数(分母一,分母二)
-    
+
     分子一变化=int(分子一*共同分母/分母一)
-    分子二变化=int(分子二*共同分母/分母二)
+    分子二变化=分子二*共同分母//分母二
     
-    if 运算法=="+":
-        分子和或差=分子一变化+分子二变化
-    else:分子和或差=分子一变化-分子二变化
-   
-    return 获取最简分数(分子和或差,共同分母)
+    分子和或差=分子一变化+分子二变化 if 运算法=="+" else 分子一变化-分子二变化
+    
+    return 获取最简分数(f"{分子和或差}/{共同分母}")
 
 
 def 分数显示形式(分数):
@@ -51,13 +47,16 @@ def 试用():
         try:
             分数一=list(map(int,input("\n分数一:").split("/")))
             if len(分数一)==1:分数一.append(1)
+        except:continue
+        try:
             分数二=list(map(int,input("分数二:").split("/")))
             if len(分数二)==1:分数二.append(1)
-            运算法=input("相加还是相减(+/-):")
+        except:continue
+        try:运算法=input("相加还是相减(+/-):")
         except:continue
         break
         
-    分数一显示,分数二显示=list(map(分数显示形式,[分数一,分数二]))
+    分数一显示,分数二显示=map(分数显示形式,(分数一,分数二))
     print("\n%s%s%s=%s"%(分数一显示,运算法,分数二显示,分数相加减(分数一,分数二,运算法)))
 
 
